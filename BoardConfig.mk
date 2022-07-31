46bbf24a43f2356913c490b455bc89bda289f29e
HRB-YuPai <64183947+HRB-YuPai@users.noreply.github.com>
2022-07-31 17:44:00 +1000

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,16 +87,16 @@ SHRP_PATH := device/xiaomi/$(SHRP_DEVICE_CODE)
 SHRP_EXPRESS := true
 SHRP_EXPRESS_USE_DATA := true
 SHRP_NOTCH := true
-SHRP_EDL_MODE := 0
+SHRP_EDL_MODE := 1
 SHRP_FLASH := 1
 SHRP_CUSTOM_FLASHLIGHT := true
 SHRP_REC := /dev/block/by-name/recovery
 INC_IN_REC_MAGISK := true
-SHRP_OFFICIAL := true
+#SHRP_OFFICIAL := true
 INC_IN_REC_ADDON_1 := true
 INC_IN_REC_ADDON_1 := true
 INC_IN_REC_ADDON_2 := true
-INC_IN_REC_ADDON_3 := false
+INC_IN_REC_ADDON_3 := true
 
 # System as root
 BOARD_SUPPRESS_SECURE_ERASE := true
